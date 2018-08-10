079dc73d19558da7b4ae472373f51b5fe3f89c00
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-10 16:14:24 +0300

    modified the object blueprint to include date. Added the date input to the quote capturing form

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,8 +9,8 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes = [
-    new Quotes (1, 'Author1', 'This is a quote.'),
-    new Quotes (1, 'Author2', 'This is yet another quote'),
+    new Quotes (1, 'Author1', 'This is a quote.', new Date (2018, 8, 10)),
+    new Quotes (1, 'Author2', 'This is yet another quote', new Date(2018,8,11)),
   ];
 
   toggleDetails(index){
