71db36d2fb0c7f9bbc05d0519b24bc30ce1171cf
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-10 12:16:48 +0300

    created a form that captures the quote's author name and the quote itself. Changed the value name to author in the object blueprint and in the other areas previously used

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
   public showQuote: boolean;
-  constructor (public id: number, public name: string, public quote: string) {
+  constructor (public id: number, public author: string, public quote: string) {
     this.showQuote = false;
   }
 }
