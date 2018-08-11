9d60b1fe0ab05986cb6b7889c22190fff9f3a567
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-11 12:10:46 +0300

    added more quotes to the quotes component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Quotes } from '../quotes';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-quotes',
@@ -9,8 +10,9 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes = [
-    new Quotes (1, 'Author1', 'This is a quote.', new Date (2018, 8, 10)),
-    new Quotes (1, 'Author2', 'This is yet another quote', new Date(2018, 8, 11)),
+    new Quotes (1, 'John Thune', 'I believe our flag is more than just cloth and ink. It is a universally recognized symbol that stands for liberty, and freedom. It is the history of our nation, and it is marked by the blood of those who died defending it.','Sam', new Date(2018,8,10)),
+    new Quotes (1, 'George Washington', 'Guard against the impostures of pretended patriotism.','Sam' new Date(2018,8,11)),
+    new Quotes (1, 'Albert Camus', 'Guard against the impostures of pretended patriotism.', 'Sam', new Date(2018,8,11)),
   ];
 
   toggleDetails(index) {
