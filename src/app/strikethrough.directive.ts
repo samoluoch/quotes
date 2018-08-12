1db99d5d0b23b5189f478b2e4759ef7f89a8995a
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-12 21:22:45 +0300

    added a function for striking through the quotes onClick

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { }
+
+  @HostListener("click") onClicks() {
+    this.textDeco("line-through")
+  }
+
+  private textDeco (action: string) {
+    this.elem.nativeElement.style.textDecoration = 'line-through';
+  }
 
 }
