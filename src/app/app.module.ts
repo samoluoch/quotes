01869f629bd1604354e6ea9c6277cdbad619c469
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-12 20:48:04 +0300

    created a custom directive for striking through the quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteStatementComponent } from './quote-statement/quote-statement.component';
 import { QuoteFormsComponent } from './quote-forms/quote-forms.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteStatementComponent,
-    QuoteFormsComponent
+    QuoteFormsComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule
